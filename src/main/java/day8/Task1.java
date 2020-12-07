46e0a92ad53a64254d22a06d1837fee5ec955a69
ilyaelagin <motortsi@gmail.com>
2020-12-07 11:48:55 +0300

    Complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,23 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start1 = System.currentTimeMillis();
+        String string = "";
+        for (int i = 0; i <= 20000; i++) {
+            string = string + i + " ";
+        }
+        long finish1 = System.currentTimeMillis();
+        long timeConsumedMillis1 = finish1 - start1;
+        System.out.println("Время работы, используя String: " + timeConsumedMillis1);
 
+        long start2 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            stringBuilder.append(i).append(" ");
+        }
+        long finish2 = System.currentTimeMillis();
+        long timeConsumedMillis2 = finish2 - start2;
+        System.out.println("Время работы, используя StringBuilder: " + timeConsumedMillis2);
     }
+
 }
