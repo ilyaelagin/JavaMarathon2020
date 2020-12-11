f6b92b03ada07c27bf2e8699449fc1af9b7e65c9
ilyaelagin <motortsi@gmail.com>
2020-12-11 21:08:50 +0300

    Change task2 day12

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -6,14 +6,12 @@ import java.util.List;
 public class Task2 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i <= 30; i++) {
+        for (int i = 0; i <= 30 || (i >= 300 && i <= 350); i++) {
             if (i % 2 == 0) {
                 list.add(i);
-            }
-        }
-        for (int i = 300; i <= 350; i++) {
-            if (i % 2 == 0) {
-                list.add(i);
+                if (i == 30) {
+                    i = 299;
+                }
             }
         }
         System.out.println(list);
