799b44f902b0c393dd3442368c10eb67b1310b5c
ilyaelagin <motortsi@gmail.com>
2020-12-17 10:46:44 +0300

    Refactor day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -5,6 +5,13 @@ import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
+/**
+ * Реализовать программу, которая на вход принимает txt файл с целыми числами  и в качестве ответа
+ * выводит в консоль среднее арифметическое этих чисел.
+ * Ответ будет являться вещественным числом.
+ * В консоль необходимо вывести полную запись вещественного числа (со всеми знаками после запятой)
+ * и сокращенную запись (с 3 знаками после запятой).
+ */
 public class Task1 {
     public static void main(String[] args) {
         File file = new File("test16.txt");
@@ -21,11 +28,13 @@ public class Task1 {
                 sum += Integer.parseInt(number);
             }
             double average = (double) sum / numbersString.length;
-            System.out.print(average + " --> " + new DecimalFormat(".000").format(average));
+            System.out.print(average + " --> " + new DecimalFormat(".000").format(average) + "\n");
+
+            // Как было в решении
+            System.out.printf(average + " --> %.3f", average);
 
         } catch (FileNotFoundException | NumberFormatException e) {
             System.out.println("Message exception: " + e.getMessage());
         }
     }
 }
-
